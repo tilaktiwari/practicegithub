fba71a91fc446682485d7166f5745c58744738e3
Tilaktiwari <tiwari5tilak@gmail.com>
2020-09-18 21:09:54 -0400

    gitpractice

---
+++ src/ZoomMeeting.java
@@ -0,0 +1,9 @@
+public class ZoomMeeting {
+    int num=4;
+    int num2=6;
+    int total=num+ num2;
+    public static void main(String[]args){
+        ZoomMeeting obj =new ZoomMeeting();
+        System.out.println("This is the total " +obj.total);
+    }
+}
